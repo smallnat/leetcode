57c3fe18808a3f6317e8c94c2e272ea84dc5d172
zhenhua.li <zhenhua.li@tongdun.net>
2022-05-30 07:02:50 +0800

    add

--- src/main/java/cn/lizh/leetcode/l204/Solution.java
+++ src/main/java/cn/lizh/leetcode/l204/Solution.java
@@ -2,7 +2,27 @@ package cn.lizh.leetcode.l204;
 
 public class Solution {
 
+    // 提供的答案也会超时。。
     public int countPrimes(int n) {
+        int ans = 0;
+        for (int i = 2; i < n; i++) {
+            ans += isPrimes(i) ? 1 : 0;
+        }
+
+        return ans;
+    }
+
+    private boolean isPrimes(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 以下结果不正确
+    public int countPrimes1(int n) {
         if (n <= 2) { // 直接能观察到的边界情况
             return 0;
         }
